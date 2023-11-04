93439a1c768f448950304712f977bcbc4c6daffa
Teja Chilumula <tejachilumula07@gmail.com>
2023-11-04 13:03:58 -0700

    Web Chat application using golang as backend and react as frontend.

---
+++ Backend/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		// Allow any origin for WebSocket connections
+		return true
+	},
+}
+
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+
+// Define a struct to hold chat messages
+type Message struct {
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
+var messages []Message
+
+func handleConnection(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Printf("Error upgrading to WebSocket: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	clients[conn] = true
+
+	log.Printf("WebSocket connection established from %s", conn.RemoteAddr())
+
+	// Send message history to the new client
+	for _, msg := range messages {
+		err := conn.WriteJSON(msg)
+		if err != nil {
+			log.Printf("Error writing WebSocket message: %v", err)
+			conn.Close()
+			delete(clients, conn)
+			return
+		}
+	}
+
+	for {
+		var msg Message
+		err := conn.ReadJSON(&msg)
+		if err != nil {
+			log.Printf("Error reading WebSocket message: %v", err)
+			delete(clients, conn)
+			break
+		}
+
+		// Store the message in memory
+		messages = append(messages, msg)
+
+		broadcast <- msg
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-broadcast
+		for client := range clients {
+			err := client.WriteJSON(msg)
+			if err != nil {
+				log.Printf("Error writing WebSocket message: %v", err)
+				client.Close()
+				delete(clients, client)
+			}
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnection)
+
+	port := 8080
+	addr := fmt.Sprintf(":%d", port)
+
+	log.Printf("Starting WebSocket server on port %d...", port)
+	go handleMessages() // Start the message broadcasting goroutine
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
---
+++ Frontend/chatapp/src/Chat.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './Chat.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '', // Username of the sender
+      message: '',  // Message input
+      messages: []  // Array to store messages
+    };
+    this.socket = new WebSocket('ws://localhost:8080/ws');
+  }
+
+  componentDidMount() {
+    const username = prompt("Please enter your username:");
+    this.setState({ username });
+
+    this.socket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      this.setState((prevState) => ({
+        messages: [...prevState.messages, message]
+      }));
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { username, message } = this.state;
+    this.socket.send(JSON.stringify({ username, message }));
+    this.setState({ message: '' });
+  }
+
+
+
+
+  // ... previous React code ...
+
+render() {
+    const { username, message, messages } = this.state;
+    return (
+      <div className="App">
+        <h1>Chat Application</h1>
+        <div className="chat-container">
+          <div className="chat-messages">
+            {messages.map((msg, index) => (
+              <div
+                key={index}
+                className={`message ${msg.username === username ? 'sender' : 'receiver'}`}
+              >
+                {msg.username !== username && (
+                  <span className={`name ${msg.username === username ? 'sender-name' : 'receiver-name'}`}>
+                    {msg.username}
+                  </span>
+                )}
+                {msg.message}
+              </div>
+            ))}
+          </div>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="Type a message..."
+              value={message}
+              onChange={(e) => this.setState({ message: e.target.value })}
+            />
+            <button type="submit">Send</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
